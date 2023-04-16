181b3491479d7acf66a9528681509a38c033fcec
indigoviolethue <aaronbowring5@gmail.com>
2023-04-16 11:18:49 +0100

    initial 2

---
+++ posts/addComment.php
@@ -0,0 +1,15 @@
+<?php
+// Get the form data
+$name = 'Anonymous'
+$comment = $_POST['comment'];
+$id = $_POST['id']
+
+// Open the CSV file for writing
+$file = fopen('comments.csv', 'a');
+$date = 
+// Append the form data to the CSV file
+fputcsv($file, array($name, $comment, $date,$id));
+
+// Close the CSV file
+fclose($file);
+?>
