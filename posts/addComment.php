934a89e581842d905e1b82370443cc69c3098f93
indigoviolethue <aaronbowring5@gmail.com>
2023-04-17 15:53:43 +0100

    adding php

--- posts/addComment.php
+++ posts/addComment.php
@@ -6,7 +6,7 @@ $id = $_POST['id']
 
 // Open the CSV file for writing
 $file = fopen('comments.csv', 'a');
-$date = 
+$date = date("d/m/Y - h:ia")
 // Append the form data to the CSV file
 fputcsv($file, array($name, $comment, $date,$id));
 
